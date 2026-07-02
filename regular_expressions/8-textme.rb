7db2df72aa29935efb1f16bfefa14ed14f121c8d
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-07-02 18:00:18 +0400

    Refreshed documentation to reflect current project state
    Changes include: Updated 8 files, Modified README.md
    
    Modified 5 lines (+4 additions, -1 deletions)

--- regular_expressions/8-textme.rb
+++ regular_expressions/8-textme.rb
@@ -1,1 +1,4 @@
 #!/usr/bin/env ruby
+puts ARGV[0].scan(//)
+
+Feb  1 11:00:00 ip-10-0-0-10 mdr: 2016-02-01 11:00:00 Sent SMS [SMSC:SYBASE1] [SVC:backendtextme] [ACT:] [BINF:] [FID:] [from:12692244605] [to:12698614402] [flags:-1:0:-1:-1:-1] [msg:99:life forms are so amazingly primitive that they still think digital watches are a pretty neat idea.] [udh:0:]
